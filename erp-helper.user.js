10fa4d7590d008167c334699e594d70fb4ec579b
Vincentliudl <82821437+Vincentliudl@users.noreply.github.com>
2026-07-16 17:32:51 +0800

    Update erp-helper.user.js

--- erp-helper.user.js
+++ erp-helper.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         GH-ERP 助手
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  try to take over the world!
 // @author       You
 // @match        http://183.134.208.28:46483/*
