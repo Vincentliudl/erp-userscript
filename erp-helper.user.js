a3f38edb729b7df4f2dcb4adc3ee8f39f4a62a97
Vincentliudl <82821437+Vincentliudl@users.noreply.github.com>
2026-07-16 17:00:35 +0800

    Update erp-helper.user.js

--- erp-helper.user.js
+++ erp-helper.user.js
@@ -7,8 +7,8 @@
 // @author       You
 // @match        http://183.134.208.28:46483/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @updateURL    https://cdn.jsdelivr.net/gh/vanjoerp/erp-userscript@main/erp-helper.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/vanjoerp/erp-userscript@main/erp-helper.user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/Vincentliudl/erp-userscript@main/erp-helper.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/Vincentliudl/erp-userscript@main/erp-helper.user.js
 // @grant        none
 // ==/UserScript==
 
